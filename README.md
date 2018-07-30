446b08433a7a016af751b99f0d91869f35ba1ac6
freewind <nowindlee@gmail.com>
2018-07-30 12:28:27 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Java Hello World Demo
-=====================
+Java Flexmark.java Demo
+=======================
+
+Render markdown file with [flexmark-java](https://github.com/vsch/flexmark-java).
 
 Run `Hello.java` in your IDE.
--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,21 @@
 package demo;
 
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.options.MutableDataSet;
+
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MutableDataSet options = new MutableDataSet();
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Node document = parser.parse("This is *Sparta*");
+        String html = renderer.render(document);
+        System.out.println(html);
     }
 
 }
